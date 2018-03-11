8c6963bb963f80cfdcc66dacc224aa75dd7f4e37
Mohammad Javad <mjavadhpour@gmail.com>
2018-03-11 22:30:03 +0330

    feat(api): shop-promotion for admin

---
+++ src/ShopPromotion.Domain/Infrastructure/Models/Form/ShopPromotionForm.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (C) 2018 Mohammad Javad HoseinPour. All rights reserved.
+// Licensed under the Private License. See LICENSE in the project root for license information.
+// Author: Mohammad Javad HoseinPour <mjavadhpour@gmail.com>
+
+namespace ShopPromotion.Domain.Infrastructure.Models.Form
+{
+    using EntityLayer;
+
+    public class ShopPromotionForm : BaseEntity
+    {
+    }
+}
